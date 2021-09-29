c722ecd2e353b43f986b345420dd7a65a3fc8a26
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-09-29 13:48:28 -0700

    added buttons that change background color

--- app.js
+++ app.js
@@ -8,3 +8,27 @@ button.addEventListener('click', ()=>{
   firstName.textContent = newName;
 });
 
+const buttonGreen = document.getElementById('green');
+const buttonBlue = document.getElementById('blue');
+const buttonPink = document.getElementById('pink');
+const footer = document.getElementById('foot');
+const header = document.getElementById('head');
+
+buttonBlue.addEventListener('click', () => {
+  header.style.backgroundColor= 'blue';
+  footer.style.backgroundColor= 'blue';
+  console.log('blue button press')
+}
+);
+
+buttonGreen.addEventListener('click', () => {
+  header.style.backgroundColor= 'green';
+  footer.style.backgroundColor= 'green';
+}
+);
+
+buttonPink.addEventListener('click', () => {
+  header.style.backgroundColor= 'pink';
+  footer.style.backgroundColor= 'pink';
+}
+);
