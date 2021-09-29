e91c67681c693c9c612efc51169e533390beb8b7
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-09-29 12:02:14 -0700

    added interactiviy to input and button

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // import functions and grab DOM elements
+const nameInput = document.getElementById('name');
+const button = document.getElementById('generate');
+const firstName = document.getElementById('firstName');
 
-// initialize global state
+button.addEventListener('click', ()=>{
+  let newName = nameInput.value;
+  firstName.textContent = newName;
+});
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
