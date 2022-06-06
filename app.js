27ab84cc08c01d90f8d23f2ed764e696f6a68c57
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-06-06 15:45:19 -0700

    cleaning up code

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
-// import functions and grab DOM elements
+/* eslint-disable indent */
 const nameInput = document.getElementById('name');
 const button = document.getElementById('generate');
 const firstName = document.getElementById('firstName');
 
-button.addEventListener('click', ()=>{
+const buttonGreen = document.getElementById('green');
+const buttonBlue = document.getElementById('blue');
+const buttonPink = document.getElementById('pink');
+
+const footer = document.getElementById('foot');
+const header = document.getElementById('head');
+
+button.addEventListener('click', () => {
   let newName = nameInput.value;
   firstName.textContent = newName;
 });
 
-const buttonGreen = document.getElementById('green');
-const buttonBlue = document.getElementById('blue');
-const buttonPink = document.getElementById('pink');
-const footer = document.getElementById('foot');
-const header = document.getElementById('head');
-
 buttonBlue.addEventListener('click', () => {
-  header.style.backgroundColor= 'blue';
-  footer.style.backgroundColor= 'blue';
-  console.log('blue button press')
-}
-);
+  header.style.backgroundColor = 'blue';
+  footer.style.backgroundColor = 'blue';
+});
 
 buttonGreen.addEventListener('click', () => {
-  header.style.backgroundColor= 'green';
-  footer.style.backgroundColor= 'green';
-}
-);
+  header.style.backgroundColor = 'green';
+  footer.style.backgroundColor = 'green';
+});
 
 buttonPink.addEventListener('click', () => {
-  header.style.backgroundColor= 'pink';
-  footer.style.backgroundColor= 'pink';
-}
-);
+  header.style.backgroundColor = 'pink';
+  footer.style.backgroundColor = 'pink';
+});
